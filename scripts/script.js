d5790e9cd079c9a2f41680cfccc2411fbfc36de5
Santa Claus <santa@northpole.com>
2019-09-01 15:31:22 +0300

    corrected NaNerror on order form

--- scripts/script.js
+++ scripts/script.js
@@ -13,11 +13,11 @@ $(document).ready(function(){
         var pizzaCrust = $('.crust option:selected').val();
 
         var pTopping = [];
-        
+
         $.each($("input[name='toppings']:checked"), function(){
             pTopping.push($(this).val());
         });
-        var pizzaTopping = pTopping.length*100;
+        var pizzaTopping = pTopping.length*200;
         var pizzaTotal = parseInt(pizzaSize) + parseInt(pizzaCrust)+ parseInt(pizzaTopping);
         alert("Your total order is: "+pizzaTotal)
         $('.delivery').show();
@@ -35,21 +35,21 @@ $(document).ready(function(){
         '<select class="form-control">'+
             '<select class="browser-default custom-select custom-select-md mb-3 name">'+
             '<option value="Chicken PeriPeri">Chicken PeriPeri</option>'+
-            '<option value="Raspberry Dessert Pizza">Hawaiian</option>'+
+            '<option value="Hawaiian">Hawaiian</option>'+
             '<option value="Chicken Tikka">Chicken Tikka</option>'+
-            '<option value="Chicken Alfredo Pizza">BBQ Chicken</option>'+
-            '<option value="Sunchoke Pizza">Club Pizza</option>'+
+            '<option value="BBQ Chicken">BBQ Chicken</option>'+
+            '<option value="Club Pizza">Club Pizza</option>'+
             '<option value="Margherita">Margherita</option>'+
           '</select>'+
         '</select>'+
     '</form>'+
 '</div>'+
 '<div class="col-md-4">'+
-    '<h4>Select your pizza crust!</h4>'+
+    '<label for="select">Select Pizza Crust</label>'+
     '<form id="form" class="crust">'+
         '<select name="Crust" class="form-control">'+
             '<option label="Crispy" -- 200" value="200"></option>'+
-            '<option label="Spicy" -- 150" value="150"></option>'+
+            '<option label="Spicy" -- 200" value="200"></option>'+
             '<option label="Gluten-free" -- 200" value="200"></option>'+
         '</select>'+
     '</div>'+
@@ -60,23 +60,23 @@ $(document).ready(function(){
     '<form id="form" class="topping">'+
         '<div class="row">'+
             '<div class="col-md-6">'+
-        '<input type="checkbox" name="toppings2" class="form control" label="Bacon" value="100">Bacon<br>'+
-        '<input type="checkbox" name="toppings2" class="form control" label="Pepperoni" value="100">Pepperoni<br>'+
-        '<input type="checkbox" name="toppings2" class="form control" label="chillies" value="100">Chillies<br>'+
-        '<input type="checkbox" name="toppings2" class="form control" label="Sausage" value="100">Sausage<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="Bacon" value="200">Bacon<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="Pepperoni" value="200">Pepperoni<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="chillies" value="200">Chillies<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="Sausage" value="200">Sausage<br>'+
     '</div>'+
         '<div class="col-md-6">'+
-        '<input type="checkbox" name="toppings2" class="form control" label="Cheese" value="100">Cheese<br>'+
-        '<input type="checkbox" name="toppings2" class="form control" label="pineapple" value="100">Pineapple<br>'+
-        '<input type="checkbox" name="toppings2" class="form control" label="Mushrooms" value="100">Mushrooms<br>'+
-        '<input type="checkbox" name="toppings2" class="form control" label="Olive" value="100">Olives<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="Cheese" value="200">Cheese<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="pineapple" value="200">Pineapple<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="Mushrooms" value="200">Mushrooms<br>'+
+        '<input type="checkbox" name="toppings2" class="form control" label="Olive" value="200">Olives<br>'+
     '</div>'+
 '</div>'+
 
     '</form>'+'<br>'+
-    
+
 '</div>'+
-'</div>');   
+'</div>');
 $(document).ready(function(){
     $('#secondOrder').click(function(){
         var pizzaSize2 = $('.size option:selected').val();
@@ -90,9 +90,9 @@ $(document).ready(function(){
 
     var pizzaTopping = pTopping2.length*100;
     var pizzaTotal = parseInt(pizzaSize2) + parseInt(pizzaCrust2)+ parseInt(pizzaTopping2);
-    
+
     alert("Your total order is: "+pizzaTotal2)//displays grandtotal for order added
-    
+
     });
     event.preventDefault();
 });
@@ -101,34 +101,3 @@ $(document).ready(function(){
 });
 
 
-//displays delivery form
-$(document).ready(function(){
-    $('button#deliver').click(function(){
-        $('.location').show();
-    });
-});
-//displays an alert for delivery locatio
-$(document).ready(function(){
-    $('form#place').submit(function(){
-        var building = $('input#building').val();
-        var office = $('input#office').val();
-        if ($('input#building').val() && $('input#office').val() !== ""){
-            alert("Your pizza will be delivered at "+building+" in house number "+office+" .");
-        } else {
-            alert("please enter your location");
-        };
-    });
-})
-//checkout
-$(document).ready(function(){
-    $('button#final').click(function(){
-        $('p#fnl').show();
-        
-        var newPizza = function(pizzaTotal, pizzaTotal2){
-            return pizzaTotal + pizzaTotal2;
-        }
-             
-        
-        $('#checkout').append('<p>'+newPizza+200+'</p>');
-    });
-});
